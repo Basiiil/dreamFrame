a186908af0ee432f412a95916664a0f324ab2b86
basiiil <reihaneh.dastres@gmail.com>
2022-12-17 00:47:56 +0330

    add headers as component

---
+++ game/src/pages/home/components/header/header.tsx
@@ -0,0 +1,32 @@
+import styles from "./header.module.css";
+import Navbar from "../../../../components/navbar";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.smallCircle}></div>
+        <div className={styles.bigCircle}></div>
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        <div className={styles.box}>
+          <h1 className={styles.title}>
+            CYbox nft <br /> collectionS <br /> for everyone
+          </h1>
+          <p className={styles.description}>
+            Nulla ornare sagittis placerat nunc sit tempus enim.
+            <br /> Accumsan pellentesque ipsum felis tristique at proin <br />{" "}
+            vel turpis.
+          </p>
+          <div className={styles.buttons}>
+            <div className={styles.wallet}>connect wallet</div>
+            <div className={styles.video}>watch video</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
